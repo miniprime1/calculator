593e2884d903ceccb3bd47e855e8b9b225ac0098
miniprime1 <64111105+miniprime1@users.noreply.github.com>
2021-02-21 11:23:43 +0900

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -4,17 +4,16 @@
 
 int main() {
 	//
-	// Background
+	// Background-process
 	//
 	system("cls");
 	system("title CLI Calculator");
 
 	//
-	// Declare
+	// Variables
 	//
-	float pi = 3.141592;
-	int opt, a, b, m, n, r;
 	float x, y, z, result;
+	int opt, n;
 	long longx;
 
 	//
@@ -36,16 +35,17 @@ int main() {
 	printf("\n6. Power functions");
 	printf("\n7. Error and gamma functions");
 	printf("\n8. Rounding and remainder functions");
-	printf("\n9. floating-point manipulation functions");
+	printf("\n9. Floating-point manipulation functions");
 	printf("\n10. Minimum, maximum, difference functions");
 	printf("\n11. Other functions");
-	printf("\n12. Exit");
+	printf("\n12. Mathematical constants");
+	printf("\n13. Exit");
 	printf("\nEnter choice: ");
 	scanf_s("%d", &opt);
 	fseek(stdin, 0, SEEK_END);
 
 	//
-	// 4 Arithmetic Operations
+	// The basic arithmetic operations
 	//
 	if (opt == 1) {
 		printf("\nOptions");
@@ -53,7 +53,7 @@ int main() {
 		printf("\n2. Subtract");
 		printf("\n3. Multiplication");
 		printf("\n4. Division");
-		printf("\nYour option: ");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -122,7 +122,7 @@ int main() {
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
-			return 0;
+			return 1;
 		}
 	}
 
@@ -139,7 +139,7 @@ int main() {
 		printf("\n6. Sector");
 		printf("\n7. Parallelogram");
 		printf("\n8. Ellipse");
-		printf("\nYour option: ");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -196,9 +196,9 @@ int main() {
 			printf("Enter radius: ");
 			scanf_s("%f", &x);
 			fseek(stdin, 0, SEEK_END);
-			result = x * x * pi;
+			result = x * x;
 			printf("\nResult");
-			printf("\n%f * %f * %f = %f", x, x, pi, result);
+			printf("\n%f * %f * pi = %f * pi", x, x, result);
 			printf("\n\n");
 			system("pause");
 			return 0;
@@ -220,9 +220,12 @@ int main() {
 			printf("Enter radius: ");
 			scanf_s("%f", &x);
 			fseek(stdin, 0, SEEK_END);
-			result = 0.5 * pow(x, 2) * 1;
+			printf("Enter degree: ");
+			scanf_s("%f", &y);
+			fseek(stdin, 0, SEEK_END);
+			result = x * x * (y/360);
 			printf("\nResult");
-			printf("\natan(%f) = %f", x, result);
+			printf("\n%f^2 * %f/360 = %f", x, y, result);
 			printf("\n\n");
 			system("pause");
 			return 0;
@@ -250,9 +253,9 @@ int main() {
 			printf("Enter axis2: ");
 			scanf_s("%f", &y);
 			fseek(stdin, 0, SEEK_END);
-			result = pi * x * y;
+			result = x * y;
 			printf("\nResult");
-			printf("\n%f * %f * %f = %f", x, x, pi, result);
+			printf("\n%f * %f * pi = %f * pi", x, x, result);
 			printf("\n\n");
 			system("pause");
 			return 0;
@@ -262,7 +265,7 @@ int main() {
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
-			return 0;
+			return 1;
 		}
 	}
 
@@ -271,14 +274,14 @@ int main() {
 	//
 	else if (opt == 3) {
 		printf("\nOptions");
-		printf("\n1. Cosine");
-		printf("\n2. Sine");
-		printf("\n3. Tangent");
-		printf("\n4. Arc Cosine");
-		printf("\n5. Arc Sine");
-		printf("\n6. Arc Tangent");
-		printf("\n7. Arc Tangent (2 parameters)");
-		printf("\nYour option: ");
+		printf("\n1. Compute cosine");
+		printf("\n2. Compute sine");
+		printf("\n3. Compute tangent");
+		printf("\n4. Compute arc cosine");
+		printf("\n5. Compute arc sine");
+		printf("\n6. Compute arc tangent");
+		printf("\n7. Compute arc tangent with two parameters");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -374,7 +377,7 @@ int main() {
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
-			return 0;
+			return 1;
 		}
 	}
 
@@ -383,13 +386,13 @@ int main() {
 	//
 	else if (opt == 4) {
 		printf("\nOptions");
-		printf("\n1. Hyperbolic Cosine");
-		printf("\n2. Hyperbolic Sine");
-		printf("\n3. Hyperbolic Tangent");
-		printf("\n4. Hyperbolic Arc Cosine");
-		printf("\n5. Hyperbolic Arc Sine");
-		printf("\n6. Hyperbolic Arc Tangent");
-		printf("\nYour option: ");
+		printf("\n1. Compute hyperbolic cosine");
+		printf("\n2. Compute hyperbolic sine");
+		printf("\n3. Compute hyperbolic tangent");
+		printf("\n4. Compute area hyperbolic cosine");
+		printf("\n5. Compute area hyperbolic sine");
+		printf("\n6. Compute area hyperbolic tangent");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -470,7 +473,7 @@ int main() {
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
-			return 0;
+			return 1;
 		}
 	}
 
@@ -484,13 +487,13 @@ int main() {
 		printf("\n3. Integer Binary Logarithm");
 		printf("\n4. Logarithm + 1");
 		printf("\n5. Binary Logarithm");
-		printf("\n6. Floating-point Base Logarithm");
+		printf("\n6. floating-point Base Logarithm");
 		printf("\n7. Exponential Function");
 		printf("\n8. Binary Exponential Function");
 		printf("\n9. Exponential - 1");
 		printf("\n10. Get significant and exponent");
 		printf("\n11. Generate value from significant and exponent");
-		printf("\nYour option: ");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -637,7 +640,7 @@ int main() {
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
-			return 0;
+			return 1;
 		}
 	}
 
@@ -650,7 +653,7 @@ int main() {
 		printf("\n2. Square root");
 		printf("\n3. Cubic root");
 		printf("\n4. Hypotenuse");
-		printf("\nYour option: ");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -713,7 +716,7 @@ int main() {
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
-			return 0;
+			return 1;
 		}
 	}
 
@@ -726,7 +729,7 @@ int main() {
 		printf("\n2. Complementary error function");
 		printf("\n3. Gamma function");
 		printf("\n4. Log-gamma function");
-		printf("\nYour option: ");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -783,7 +786,7 @@ int main() {
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
-			return 0;
+			return 1;
 		}
 	}
 
@@ -804,7 +807,7 @@ int main() {
 		printf("\n10. Round and cast to long long integer");
 		printf("\n11. Round to nearby integral value");
 		printf("\n12. Remainder (IEC 60559)");
-		printf("\nYour option: ");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -963,19 +966,19 @@ int main() {
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
-			return 0;
+			return 1;
 		}
 	}
 
 	//
-	// floating-point manipulation functions
+	// Floating-point manipulation functions
 	//
 	if (opt == 9) {
 		printf("\nOptions");
 		printf("\n1. Copy sign");
 		printf("\n2. Next representable value");
 		printf("\n3. Next representable value toward precise value");
-		printf("\nYour option: ");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -1029,7 +1032,7 @@ int main() {
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
-			return 0;
+			return 1;
 		}
 	}
 
@@ -1041,7 +1044,7 @@ int main() {
 		printf("\n1. Positive difference");
 		printf("\n2. Maximum value");
 		printf("\n3. Minimum value");
-		printf("\nYour option: ");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -1095,7 +1098,7 @@ int main() {
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
-			return 0;
+			return 1;
 		}
 	}
 
@@ -1107,7 +1110,8 @@ int main() {
 		printf("\n1. Compute absolute value (fabs)");
 		printf("\n2. Compute absolute value (abs)");
 		printf("\n3. Multiply-add");
-		printf("\nYour option: ");
+		printf("\n4. Average");
+		printf("\nEnter choice: ");
 		scanf_s("%d", &opt);
 		fseek(stdin, 0, SEEK_END);
 
@@ -1153,19 +1157,147 @@ int main() {
 			system("pause");
 			return 0;
 		}
+		else if (opt == 3) {
+			printf("\n");
+			printf("Enter 1st input: ");
+			scanf_s("%f", &x);
+			fseek(stdin, 0, SEEK_END);
+			printf("Enter 2nd input: ");
+			scanf_s("%f", &y);
+			fseek(stdin, 0, SEEK_END);
+			result = fmin(x, y);
+			printf("\nResult");
+			printf("\nfmin(%f, %f) = %f", x, y, result);
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
+		else if (opt == 4) {
+			printf("\n");
+			printf("Enter 1st input: ");
+			scanf_s("%f", &x);
+			fseek(stdin, 0, SEEK_END);
+			printf("Enter 2nd input: ");
+			scanf_s("%f", &y);
+			fseek(stdin, 0, SEEK_END);
+			result = (x + y) / 2;
+			printf("\nResult");
+			printf("\n%f + %f / 2 = %f", x, y, result);
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
 		else {
 			printf("\n");
 			printf("Error: invalid choice");
 			printf("\n\n");
 			system("pause");
+			return 1;
+		}
+	}
+
+	//
+	// Mathematical constants
+	//
+	else if (opt == 14) {
+		printf("\nOptions");
+		printf("\n1. Pi");
+		printf("\n2. e");
+		printf("\n3. i");
+		printf("\n4. Square root of 2");
+		printf("\n5. Square root of 3");
+		printf("\n6. Square root of 5");
+		printf("\n7. Euler–Mascheroni constant");
+		printf("\n8. Golden ratio");
+		printf("\n9. Bernstein's constant");
+		printf("\n10. Omega constant");
+		printf("\nEnter choice: ");
+		scanf_s("%d", &opt);
+		fseek(stdin, 0, SEEK_END);
+
+		if (opt == 1) {
+			printf("\nResult");
+			printf("\npi = 3.14159265358979323846264338327950288");
+			printf("\n\n");
+			system("pause");
 			return 0;
+		}
+		else if (opt == 2) {
+			printf("\nResult");
+			printf("\ne = 271828182845904523536028747135266249");
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
+		else if (opt == 3) {
+			printf("\nResult");
+			printf("\ni = sqrt(-1) [nan]");
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
+		else if (opt == 4) {
+			printf("\nResult");
+			printf("\nsqrt(2) = 141421356237309504880168872420969807");
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
+		else if (opt == 5) {
+			printf("\nResult");
+			printf("\nsqrt(3) = 1732050807568877293527446341505872368");
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
+		else if (opt == 6) {
+			printf("\nResult");
+			printf("\nsqrt(5) = 2.23606797749978969640917366873127623");
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
+		else if (opt == 7) {
+			printf("\nResult");
+			printf("\ngamma = 0.57721566490153286060651209008240243");
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
+		else if (opt == 8) {
+			printf("\nResult");
+			printf("\nphi = 1.61803398874989484820458683436563811");
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
+		else if (opt == 9) {
+			printf("\nResult");
+			printf("\nbeta = 0.28016949902386913303");
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
+		if (opt == 10) {
+			printf("\nResult");
+			printf("\nomega = 0.56714329040978387299996866221035554");
+			printf("\n\n");
+			system("pause");
+			return 0;
+		}
+		else {
+			printf("\n");
+			printf("Error: invalid choice");
+			printf("\n\n");
+			system("pause");
+			return 1;
 		}
 	}
 
 	//
 	// Exit
 	//
-	else if (opt == 12) {
+	else if (opt == 13) {
 		return 0;
 	}
 
@@ -1177,7 +1309,7 @@ int main() {
 		printf("Error: invalid choice");
 		printf("\n\n");
 		system("pause");
-		return 0;
+		return 1;
 	}
 
 	return 0;
